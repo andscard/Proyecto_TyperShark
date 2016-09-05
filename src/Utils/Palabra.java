d601deffc0266d3c701d63567667361f1d7b418f
Mayken95 <mayu-satu@hotmail.com>
2016-09-05 12:25:14 -0500

    Reestructuración de la clase Palabra

--- src/Utils/Palabra.java
+++ src/Utils/Palabra.java
@@ -5,179 +5,70 @@
  */
 package Utils;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 /**
  *
- * @author User
+ * @author Mayken
  */
-public class Palabra {
-    private ArrayList<String> palabras ;
-    private ArrayList<String> bonus ;
-    private ArrayList<String> letras ;
-    private String archivo_palabras="ListaPalabras.txt";
-    private String archivo_bonus;
-    
-     public Palabra(){
-        
-           this.palabras=this.llenarArregloPalabras(this.archivo_palabras);;
-           this.letras=this.llenarArregloLetras();
-        
-    }
-    
-    public ArrayList<String> cargar (String archivo) {
-        try {
-        File reviewFile = new File(archivo);
-        ArrayList<String> lineas = new ArrayList<>();
-        Scanner reviewScanner = new Scanner(reviewFile);
-        
-        String palabra;
-
-        while(reviewScanner.hasNext())
-        {
-            palabra = reviewScanner.nextLine();
-            lineas.add(palabra.toLowerCase());
-        }
-        
-         return  lineas; 
-        
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(Palabra.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
-        } 
-    }
-    
-    
-    
-    
-    public ArrayList<String> llenarArregloPalabras(String archivo ){
-        ArrayList<String> lineas=this.cargar(archivo);
-        ArrayList<String> palabras=new ArrayList<String>();
-        ArrayList<Character> letras =new ArrayList<Character>();
-       
-        int generar, cont=0;
-        
-        while(cont<5){
-                    generar=this.generarAleatorio(lineas.size(),0);
-                    if(!letras.contains(lineas.get(generar).charAt(0))){
-                         letras.add(lineas.get(generar).charAt(0));
-                         palabras.add(lineas.get(generar));
-                         lineas.remove(generar);
-                         cont=cont+1;}
-                    
-        }
-                return palabras;
-    }
-    
-   
-    public ArrayList<String> llenarArregloLetras(){
-        ArrayList<String> letras = new ArrayList<String>();
-        ArrayList<String> abecedario = this.llenarArregloAbecedario();
-        int generar;
-        
-       
-        for(int i=0;i<10;i++){
-                    generar=this.generarAleatorio(abecedario.size(),0);
-                    letras.add(abecedario.get(generar));
-                    abecedario.remove(generar);
-        }
-                return letras;
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-       
-    public int generarAleatorio(int num_final, int num_inicial){
-             Random  random = new Random();
-             int generar;
-             generar=(int) (random.nextDouble() * num_final + num_inicial);
-             return generar;
-    }
-    
-    
-      public ArrayList<String> llenarArregloAbecedario(){
+public class Palabra{
+    String palabra;
+     Label []label;
      
-            ArrayList<String> letras =new ArrayList<String>();
-            
-            letras.add("A");
-            letras.add("B");
-            letras.add("C");
-            letras.add("D");
-            letras.add("E");
-            letras.add("F");
-            letras.add("G");
-            letras.add("H");
-            letras.add("I");
-            letras.add("J");
-            letras.add("K");
-            letras.add("L");
-            letras.add("M");
-            letras.add("N");
-            letras.add("O");
-            letras.add("P");
-            letras.add("Q");
-            letras.add("R");
-            letras.add("S");
-            letras.add("T");
-            letras.add("U");
-            letras.add("V");
-            letras.add("W");
-            letras.add("X");
-            letras.add("Y");
-            letras.add("Z");
+     
+    public Palabra(String palabra) {
+        this.palabra = palabra;
+        this.llenarArregloLabels();
+        
+    }
+     
+    
+    public int  getPalabra() {
+        return palabra.length();
+    }
+
+    
+    public Label[] getLabel() {
+        return label;
+    }
+   
+
+    
+    
+        
+    public void llenarArregloLabels(){
+         label= new Label[this.palabra.length()];
+         String pal;         
+         for( int i=0;i<palabra.length();i++){
+             pal=String.valueOf(palabra.charAt(i));
+              label[i]=new Label(pal);  
+              label[i].setFont(new Font("Arial", 20));
+              label[i].setTextFill(Color.BLUE);
              
-     return letras;
-     }       
-   
-    
-    
-    
-    
-
-    /**
-     * @return the palabras
-     */
-    public ArrayList<String> getPalabras() {
-        return palabras;
+            }
     }
-
-    /**
-     * @param palabras the palabras to set
-     */
-    public void setPalabras(ArrayList<String> palabras) {
-        this.palabras = palabras;
+    
+    public void cambiarColorLetras( int posicion ){
+                 label[posicion].setTextFill(Color.GREEN);
+          
     }
-
-    /**
-     * @return the letras
-     */
-    public ArrayList<String> getLetras() {
-        return letras;
-    }
-
-    /**
-     * @param letras the letras to set
-     */
-    public void setLetras(ArrayList<String> letras) {
-        this.letras = letras;
-    }
-     
-
     
     
     
+    public HBox arregloLabels(){
+         HBox palabra = new HBox();
+         palabra.getChildren().addAll(label);
+                 
+         
+         return palabra;
+    }
         
+         
+    
+  
+    
 }
